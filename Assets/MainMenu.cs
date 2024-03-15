d27660b9be5fe2e97bd8b35a5d66ec0736006768
Gonçalo Ferreira <goncaloferreira2408@gmail.com>
2024-03-15 00:03:06 +0000

    improved

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,9 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject instructions;
+    public GameObject backButton;
+
+    public GameObject playButton;
+    public GameObject settingsButton;
+    public GameObject quitButton;
+
+    //start
+    void Start()
+    {
+        instructions.SetActive(false);
+        backButton.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ShowInstructions()
+    {
+        instructions.SetActive(true);
+        backButton.SetActive(true);
+        playButton.SetActive(false);
+        settingsButton.SetActive(false);
+        quitButton.SetActive(false);
+    }
+
+    public void HideInstructions()
+    {
+        instructions.SetActive(false);
+        backButton.SetActive(false);
+        playButton.SetActive(true);
+        settingsButton.SetActive(true);
+        quitButton.SetActive(true);
     }
 
     public void QuitGame()
